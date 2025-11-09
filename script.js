974b0856b34d5fc37c5397a8749d9679fbb5b1cd
spencerowen-collab <spencerowen@berkeley.edu>
2025-11-08 18:51:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ map.on('load', function() {
         <div>
             <h3>${properties.Landmark}</h3>
             <p><strong>Address:</strong> ${properties.Address}</p>
-            <p><strong>Architect & Date:</strong> ${properties.Architect and Date}</p>
+            <p><strong>Architect & Date:</strong> ${properties.ArchitectandDate}</p>
             </p>${properties.Link ? `<p><a href="${properties.Link}" target="_blank">More Information</a></p>` : ''}
             ${properties.Notes ? `<p><strong>Notes:</strong> ${properties.Notes}</p>` : ''}
         </div>
